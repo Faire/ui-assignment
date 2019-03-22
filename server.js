0217b28f9a1791fedaa841e828e685cdda673fca
James Blashill <james@faire.com>
2019-03-21 20:06:46 -0400

    add basic request example using proxy

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 const proxy = require("http-proxy-middleware");
+
 const app = express();
 
 app.use(
-  "*",
+  "/api",
   proxy({ target: "https://www.faire.com", secure: false, changeOrigin: true })
 );
+
+app.use("/", express.static(path.resolve("public")));
 
 app.listen(4242, err => {
   if (err) {
